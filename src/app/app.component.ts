2dca4b19c6b6d2613b2c66cfd7d6bee8256f71d9
namnd <dinhnambk96@gmail.com>
2021-07-11 06:58:13 +0700

    demo switch map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonService } from './common.service';
+import { switchMap, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo-rxjs';
+
+  constructor(private commonService: CommonService) {}
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    this.commonService
+      .api1(1)
+      .pipe(
+        switchMap((data) => {
+          return this.commonService.api2(data[0].id);
+        })
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+
+  getData2(): void {
+    this.commonService
+      .api1(1)
+      .pipe(
+        switchMap((data) => {
+          return this.commonService.api2(data[0].id);
+        })
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
 }
